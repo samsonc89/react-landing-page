2aa136a202c35fbbb9012edcc30628f67bbcaaa4
Samson Cheung <samsonc89@yahoo.com>
2023-03-16 10:08:45 -0700

    Add basic elements to header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 import "./header.css";
+import ai from "../../assets/ai.png";
+import people from "../../assets/people.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header__container">
+      <div className="header__content">
+        <h1 className="header__content-heading gradient__text">
+          Let's Build Something amazing with GPT-3 Open AI
+        </h1>
+        <p className="header__content-description">
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <form>
+          <label>
+            <input type="text" name="name" placeholder="Your Email" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <div className="header__content-signup-stat">
+          <img src={people} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+      <div className="header__illustration">
+        <img src={ai} alt="ai" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
