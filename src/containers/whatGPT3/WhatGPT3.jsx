a276bd1ca3a6642da00bf0bc5c6df4f7e9491a88
Samson Cheung <samsonc89@yahoo.com>
2023-03-21 12:00:23 -0700

    Add text to features

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import "./whatGPT3.css";
+import Feature from "../../components/feature/Feature";
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  return (
+    <div className="wgpt3__container">
+      <Feature
+        title="Hello"
+        text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+      />
+      <div className="wgpt3__heading">
+        <h2 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h2>
+        <p>
+          <a href="">Explore The Library</a>
+        </p>
+      </div>
+      <Feature
+        title="Chatbots"
+        text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. "
+      />
+      <Feature
+        title="Knowledgebase"
+        text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+      />
+      <Feature
+        title="Education"
+        text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+      />
+    </div>
+  );
 };
 
 export default WhatGPT3;
