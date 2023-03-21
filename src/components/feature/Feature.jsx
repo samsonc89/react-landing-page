5284581467cebfa7809a8f2874b4163d20ecab63
Samson Cheung <samsonc89@yahoo.com>
2023-03-21 12:59:41 -0700

    Add className prop to be passed in to features

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./feature.css";
 
-const Feature = ({ title, text }) => {
+const Feature = ({ title, text, className }) => {
   return (
-    <div className="feature__container">
+    <div className={`feature__container ${className}`}>
       <div className="feature__title-container">
         <div className="feature__title-bar"></div>
         <h2 className="feature__title-title">{title}</h2>
