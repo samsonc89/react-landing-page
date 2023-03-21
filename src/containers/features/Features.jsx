1f2b9b90262a4d102068b5f3e93a61309a31e9f7
Samson Cheung <samsonc89@yahoo.com>
2023-03-21 14:59:10 -0700

    Create basic structure for Features area

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,8 +1,37 @@
 import React from "react";
+import Feature from "../../components/feature/Feature";
 import "./features.css";
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div className="features__container section__padding">
+      <div className="features__heading__container">
+        <h1 className="gradient__text">
+          The Future is Now and You Just Need To Realize It. Step into Future
+          Today & Make it Happen.
+        </h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className="features__features__container">
+        <Feature
+          title="Improving end distrusts instantly "
+          text="From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded."
+        />
+        <Feature
+          title="Become the tended active"
+          text="Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to."
+        />
+        <Feature
+          title="Message or am nothing"
+          text="Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address."
+        />
+        <Feature
+          title="Really boy law county"
+          text="Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush."
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Features;
