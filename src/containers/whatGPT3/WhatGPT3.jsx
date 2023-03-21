afc1db2d84e3e608a0678a03ebcb33c84de77abf
Samson Cheung <samsonc89@yahoo.com>
2023-03-21 12:02:52 -0700

    Add id to section container

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -4,7 +4,7 @@ import Feature from "../../components/feature/Feature";
 
 const WhatGPT3 = () => {
   return (
-    <div className="wgpt3__container">
+    <div className="wgpt3__container" id="wgpt3">
       <Feature
         title="Hello"
         text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
