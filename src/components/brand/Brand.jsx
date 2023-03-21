61cb8cbd75b83dd7a4c676a632d9645be2dd59b1
Samson Cheung <samsonc89@yahoo.com>
2023-03-20 18:12:22 -0700

    Add company logos to render

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./brand.css";
+import google from "../../assets/google.png";
+import slack from "../../assets/slack.png";
+import atlassian from "../../assets/atlassian.png";
+import dropbox from "../../assets/dropbox.png";
+import shopify from "../../assets/shopify.png";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="brand__container">
+      <img src={google} />
+      <img src={slack} />
+      <img src={atlassian} />
+      <img src={dropbox} />
+      <img src={shopify} />
+    </div>
+  );
 };
 
 export default Brand;
