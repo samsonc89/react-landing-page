c00687ab57658a8233b939304f1f8db6cb05fde1
Samson Cheung <samsonc89@yahoo.com>
2023-03-22 14:14:23 -0700

    Clean up files

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import blog01 from "./blog01.png";
+import blog02 from "./blog02.png";
+import blog03 from "./blog03.png";
+import blog04 from "./blog04.png";
+import blog05 from "./blog05.png";
+
+export { blog01, blog02, blog03, blog04, blog05 };
