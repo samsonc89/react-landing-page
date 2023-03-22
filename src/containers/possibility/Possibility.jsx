250094c4d1732f8333b010d287ce7294f7ac002d
Samson Cheung <samsonc89@yahoo.com>
2023-03-22 09:58:39 -0700

    Add text elements to possiblity section

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import "./possibility.css";
+import possibility from "../../assets/possibility.png";
 
 const Possibility = () => {
-  return <div>Possibility</div>;
+  return (
+    <div className="possibility__container">
+      <div className="possibility__image">
+        <img src={possibility} alt="possibility" />
+      </div>
+      <div className="possibility__text">
+        <p className="possibility__text-early-access-1">
+          Request Early Access to Get Started
+        </p>
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p className="possibility__text-description">
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <p className="possibility__text-early-access-2">
+          Request Early Access to Get Started
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Possibility;
