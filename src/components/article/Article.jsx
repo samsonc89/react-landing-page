4b5ff186d954d0cf8e35e79ef6ea2683497c34f8
Samson Cheung <samsonc89@yahoo.com>
2023-03-22 11:15:34 -0700

    Create basic structure for articles

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./article.css";
 
-const Article = () => {
-  return <div>Article</div>;
+const Article = ({ image, altText, blogDate, blogTitle, blogLink }) => {
+  return (
+    <div className="article__container">
+      <div className="article__image">
+        <img src={image} alt={altText} />
+      </div>
+      <div className="article__description">
+        <div className="article__description-date">{blogDate}</div>
+        <div className="article__description-title">{blogTitle}</div>
+        <div className="article__description-link">
+          <a href={blogLink}>Read Full Article</a>{" "}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
