0eaeff969a6e68b61153dfc6c1308336558a6e27
Samson Cheung <samsonc89@yahoo.com>
2023-03-22 13:53:37 -0700

    Adjust spacing for all text in footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./footer.css";
+import logo from "../../assets/logo.svg";
 
 const Footer = () => {
   return (
-    <div className="footer__container section__padding">
+    <div className="footer__container">
       <div className="footer__heading">
         <p className="gradient__text">
           Do you want to step in to the future before others
@@ -12,25 +13,27 @@ const Footer = () => {
       <button>Request Early Access</button>
       <div className="footer__links-container">
         <div className="footer__links-logo__container">
-          <div className="footer__links-logo"></div>
+          <div className="footer__links-logo">
+            <img src={logo} alt="logo" />
+          </div>
           <div className="footer__links-logo-text">
             <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
           </div>
         </div>
-        <div className="footer__links-links">
+        <div className="footer__links-section">
           <h4>Links</h4>
           <p>Overons</p>
           <p>Social Media</p>
           <p>Counters</p>
           <p>Contact</p>
         </div>
-        <div className="footer__links-company">
+        <div className="footer__links-section">
           <h4>Company</h4>
           <p>Terms & Conditions</p>
           <p>Privacy Policy</p>
           <p>Contact</p>
         </div>
-        <div className="footer__links-contact">
+        <div className="footer__links-section">
           <h4>Get in touch</h4>
           <p>Crechterwoord K12 182 DK Alknjkcb</p>
           <p>085-132567</p>
