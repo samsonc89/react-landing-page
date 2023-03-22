c9fc216568a01e53beb7d8d92e7ea1bafb5dc7f0
Samson Cheung <samsonc89@yahoo.com>
2023-03-22 10:52:32 -0700

    Create call to action component

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./cta.css";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <div className="cta__container section__margin">
+      <div className="cta__text__container">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <button type="button">Get Started</button>
+    </div>
+  );
 };
 
 export default CTA;
