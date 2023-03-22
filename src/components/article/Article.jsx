1d3f605aac2f3b044018487ec0cda2b0961ffc51
Samson Cheung <samsonc89@yahoo.com>
2023-03-22 11:49:34 -0700

    Add default text in props

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import "./article.css";
 
-const Article = ({ image, altText, blogDate, blogTitle, blogLink }) => {
+const Article = ({
+  image,
+  altText = "blog image",
+  blogDate = "Sep 26, 2021",
+  blogTitle = "GPT-3 and Open  AI is the future. Let us exlore how it is?",
+  blogLink,
+}) => {
   return (
     <div className="article__container">
       <div className="article__image">
         <img src={image} alt={altText} />
       </div>
       <div className="article__description">
-        <div className="article__description-date">{blogDate}</div>
+        <div className="article__description-date">
+          <p>{blogDate}</p>
+        </div>
         <div className="article__description-title">{blogTitle}</div>
         <div className="article__description-link">
-          <a href={blogLink}>Read Full Article</a>{" "}
+          <p>
+            <a href={blogLink}>Read Full Article</a>{" "}
+          </p>
         </div>
       </div>
     </div>
